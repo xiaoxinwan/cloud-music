296e77111b8e42e55ba0aaaba20be090c27a0804
xiaoxinwan <lixinxi1010@gmail.com>
2018-09-28 18:43:50 +0800

    点击歌曲时，激活该歌曲

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -8,12 +8,20 @@
         render(data) {
             let $el = $(this.el)
             $el.html(this.template)
-            let { songs } = data
-            let liList = songs.map((song)=> $('<li></li>').text(song.name))
+            let {
+                songs
+            } = data
+            let liList = songs.map((song) => 
+                $('<li></li>').text(song.name).attr('data-song-id', song.id))
             $el.find('ul').empty()
-            liList.map((domLi)=>{
+            liList.map((domLi) => {
                 $el.find('ul').append(domLi)
             })
+        },
+        activeItem(li){
+            let $li = $(li)
+            $li.addClass('active')
+                .siblings('.active').removeClass('active') 
         },
         clearActive() {
             $(this.el).find('.active').removeClass('active')
@@ -21,13 +29,16 @@
     }
     let model = {
         data: {
-            songs: [ ]
+            songs: []
         },
-        find(){
+        find() {
             var query = new AV.Query('Song');
-            return query.find().then((songs)=>{
-                this.data.songs = songs.map((song)=>{
-                    return {id: song.id, ...song.attributes}
+            return query.find().then((songs) => {
+                this.data.songs = songs.map((song) => {
+                    return {
+                        id: song.id,
+                        ...song.attributes
+                    }
                 })
                 return songs
             })
@@ -38,16 +49,29 @@
             this.view = view
             this.model = model
             this.view.render(this.model.data)
+            this.bindEvents()
+            this.getAllsongs()
+            this.bindEventHub()
+            
+        },
+        getAllsongs() {
+            return this.model.find().then(() => {
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents() {
+            $(this.view.el).on('click','li',(e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select', {id: songId})
+            })
+        },
+        bindEventHub() {
             window.eventHub.on('upload', () => {
                 this.view.clearActive()
             })
             window.eventHub.on('create', (songData) => {
                 this.model.data.songs.push(songData)
-                this.view.render(this.model.data)
-            })
-            this.model.find().then(()=>{
-                console.log('llllllllllllllllllllllllll')
-                console.log(this.model.data)
                 this.view.render(this.model.data)
             })
         }
