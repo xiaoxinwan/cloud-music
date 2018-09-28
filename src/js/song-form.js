f35735b87689096127811cafb2a2935bacde44ce
xiaoxinwan <lixinxi1010@gmail.com>
2018-09-28 22:28:47 +0800

    更新数据后刷新视图

--- src/js/song-form.js
+++ src/js/song-form.js
@@ -53,6 +53,16 @@
             url: '',
             id: ''
         },
+        update(data) {
+            var song = AV.Object.createWithoutData('Song', this.data.id)
+            song.set('name', data.name)
+            song.set('artist', data.artist)
+            song.set('url', data.url)
+            return song.save().then((response) => {
+                Object.assign(this.data, data)
+                return response
+            })
+        },
         create(data) {
             var Song = AV.Object.extend('Song');
             var song = new Song();
@@ -85,34 +95,60 @@
                 this.view.render(this.model.data)
             })
             window.eventHub.on('new', (data) => {
-                if(this.model.data.id){
+                if (this.model.data.id) {
                     this.model.data = {
                         name: '',
                         url: '',
                         artist: '',
                         id: ''
                     }
-                }else{
+                } else {
                     Object.assign(this.model.data, data)
                 }
+                console.log('xxxxxxxxxxxxxxxxxxxxx')
+                console.log(this.model.data)
+
                 this.view.render(this.model.data)
             })
+        },
+        create() {
+            let needs = 'name artist url'.split(' ')
+            let data = {}
+            needs.map((string) => {
+                data[string] = this.view.$el.find(`[name = "${string}"]`).val()
+            })
+            this.model.create(data)
+                .then(() => {
+                    this.view.reset()
+                    let string = JSON.stringify(this.model.data)
+                    let object = JSON.parse(string)
+                    window.eventHub.emit('create', object)
+                })
+        },
+        update(){
+            let needs = 'name artist url'.split(' ')
+            let data = {}
+            needs.map((string) => {
+                data[string] = this.view.$el.find(`[name = "${string}"]`).val()
+            })
+            this.model.update(data)
+                .then(()=>{
+                    window.eventHub.emit('update', JSON.parse(JSON.stringify(this.model.data)))
+                })
         },
         bindEvents() {
             this.view.$el.on('submit', 'form', (e) => {
                 e.preventDefault()
-                let needs = 'name artist url'.split(' ')
-                let data = {}
-                needs.map((string) => {
-                    data[string] = this.view.$el.find(`[name = "${string}"]`).val()
-                })
-                this.model.create(data)
-                    .then(() => {
-                        this.view.reset()
-                        let string = JSON.stringify(this.model.data)
-                        let object = JSON.parse(string)
-                        window.eventHub.emit('create', object)
-                    })
+
+                if(this.model.data.id){
+                    this.update()
+                }else{
+                    this.create()
+                }
+
+
+
+
             })
 
 
