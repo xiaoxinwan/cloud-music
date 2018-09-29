428af320a2e67c34b0b807bfbbfafe0905971d90
xiaoxinwan <lixinxi1010@gmail.com>
2018-09-29 10:00:16 +0800

    修复编辑之后，被编辑的歌曲高亮

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -9,19 +9,21 @@
             let $el = $(this.el)
             $el.html(this.template)
             let {
-                songs
+                songs,
+                selectedSongId
             } = data
-            let liList = songs.map((song) => 
-                $('<li></li>').text(song.name).attr('data-song-id', song.id))
+            let liList = songs.map((song) => {
+                let $li = $('<li></li>').text(song.name)
+                    .attr('data-song-id', song.id)
+                if (song.id === selectedSongId) {
+                    $li.addClass('active')
+                }
+                return $li
+            })
             $el.find('ul').empty()
             liList.map((domLi) => {
                 $el.find('ul').append(domLi)
             })
-        },
-        activeItem(li){
-            let $li = $(li)
-            $li.addClass('active')
-                .siblings('.active').removeClass('active') 
         },
         clearActive() {
             $(this.el).find('.active').removeClass('active')
@@ -29,7 +31,8 @@
     }
     let model = {
         data: {
-            songs: []
+            songs: [],
+            selectedSongId: undefined,
         },
         find() {
             var query = new AV.Query('Song');
@@ -59,13 +62,16 @@
             })
         },
         bindEvents() {
-            $(this.view.el).on('click','li',(e)=>{
-                this.view.activeItem(e.currentTarget)
+            $(this.view.el).on('click', 'li', (e) => {
                 let songId = e.currentTarget.getAttribute('data-song-id')
-                let data 
+                
+                this.model.data.selectedSongId = songId
+                this.view.render(this.model.data)
+                
+                let data
                 let songs = this.model.data.songs
-                for(let i =0; i< songs.length;i++){
-                    if(songs[i].id === songId){
+                for (let i = 0; i < songs.length; i++) {
+                    if (songs[i].id === songId) {
                         data = songs[i]
                         break
                     }
@@ -78,13 +84,13 @@
                 this.model.data.songs.push(songData)
                 this.view.render(this.model.data)
             })
-            window.eventHub.on('new', ()=>{
+            window.eventHub.on('new', () => {
                 this.view.clearActive()
             })
-            window.eventHub.on('update',(song)=>{
+            window.eventHub.on('update', (song) => {
                 let songs = this.model.data.songs
-                for(let i = 0; i< songs.length;i++){
-                    if (songs[i].id === song.id){
+                for (let i = 0; i < songs.length; i++) {
+                    if (songs[i].id === song.id) {
                         Object.assign(songs[i], song)
                     }
                 }
