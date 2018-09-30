72e6d1668801f76c036cd7327c1f3016dd9b7b19
xiaoxinwan <lixinxi1010@gmail.com>
2018-09-30 16:19:33 +0800

    获取Id 添加播放暂停

--- src/js/index/page-1-2.js
+++ src/js/index/page-1-2.js
@@ -4,25 +4,30 @@
         init() {
             this.$el = $(this.el)
         },
+        template:`
+        <li>
+            <h3>{{song.name}}</h3>
+            <p>
+                <svg class="icon icon-sq">
+                    <use xlink:href="#icon-ttpodicon"></use>
+                </svg>
+                {{song.artist}}
+            </p>
+            <a class="playButton" href="./song.html?id={{song.id}}">
+                <svg class="icon icon-play">
+                    <use xlink:href="#icon-bofang"></use>
+                </svg>
+            </a>
+        </li>
+        `,
         render(data) {
             let {songs} = data
             songs.map((song) => {
-                let $li = $(`
-                <li>
-                    <h3>${song.name}</h3>
-                    <p>
-                        <svg class="icon icon-sq">
-                            <use xlink:href="#icon-ttpodicon"></use>
-                        </svg>
-                    ${song.artist}
-                    </p>
-                    <a class="playButton" href="#">
-                        <svg class="icon icon-play">
-                            <use xlink:href="#icon-bofang"></use>
-                        </svg>
-                    </a>
-                </li>
-                `)
+                let $li = $(this.template
+                    .replace('{{song.name}}', song.name)
+                    .replace('{{song.artist}}', song.artist)
+                    .replace('{{song.id}}', song.id)
+                )
                 this.$el.find('ol.list').append($li)
             })
         }
